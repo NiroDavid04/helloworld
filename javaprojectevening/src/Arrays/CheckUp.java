ac03d0e56174c11c164f974094d9d88d7e181b2c
hp <hp@Nirodavid-PC.lan>
2022-12-16 18:39:47 +0530

    Checkup

---
+++ javaprojectevening/src/Arrays/CheckUp.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class CheckUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This just checkup for after editing it will push into github");
+	}
+
+}
